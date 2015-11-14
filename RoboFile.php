22ad6afbc0cf4a8e62455a116a6a6d3aca84085e
Niels Braczek <nbraczek@bsds.de>
2015-11-14 18:09:38 +0100

    Added show:config command

--- RoboFile.php
+++ RoboFile.php
@@ -20,4 +20,33 @@ class RoboFile extends \Robo\Tasks
             }
         }
     }
+
+    /**
+     * Show the current configuration settings
+     *
+     * @param string $subset Restrict the output to configuration values for a specific section
+     */
+    public function showConfig($subset = null, $options = [])
+    {
+        $pattern = empty($subset) ? '~^RoboFile\.(.*)$~' : $pattern = '~^RoboFile(\.' . $subset . '\..*)$~i';
+
+        if ($options['no-ansi']) {
+            $format = "%-32s %s";
+        } else {
+            $format = "<fg=yellow>%-32s</fg=yellow> <fg=green>%s</fg=green>";
+        }
+
+        $config = new ReflectionProperty('\Robo\Config', 'config');
+        $config->setAccessible(true);
+
+        foreach ($config->getValue() as $key => $value) {
+            if (!preg_match($pattern, $key, $match)) {
+                continue;
+            }
+            if (!is_scalar($value)) {
+                $value = gettype($value);
+            }
+            $this->say(sprintf($format, $match[1], $value));
+        }
+    }
 }
